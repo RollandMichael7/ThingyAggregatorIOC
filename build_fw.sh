42d9640d34437ee9cf81f08cb0fc238cc6753db9
Michael Rolland <rollandmichael7@gmail.com>
2020-01-16 14:11:52 -0500

    Fix SDK folder name in build_fw.sh

--- build_fw.sh
+++ build_fw.sh
@@ -2,14 +2,13 @@
 
 echo Cloning SDK...
 git clone https://github.com/RollandMichael7/nrf-sdk-v15.3
-echo
 echo Done.
+echo
 
-cd nrf-sdk-v15/examples
+cd nrf-sdk-v15.3/examples
 mkdir training
 cd training
 
 echo Cloning aggregator firmware...
 git clone https://github.com/RollandMichael7/nrf52-ble-multi-link-multi-role/
-echo
 echo Done.
